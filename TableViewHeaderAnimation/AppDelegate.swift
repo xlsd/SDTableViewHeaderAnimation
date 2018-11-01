d9ef1f2007ec77dcab8eab84dd3a43cc67a6843a
as_one <linxue2048@gmail.com>
2018-11-01 17:17:54 +0800

    [SDTableViewHeaderAnimation]add navigationController

--- TableViewHeaderAnimation/AppDelegate.swift
+++ TableViewHeaderAnimation/AppDelegate.swift
@@ -18,13 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = AnimationViewController()
+        self.window?.rootViewController = UINavigationController.init(rootViewController: AnimationViewController());
         self.window?.backgroundColor = UIColor.white
         self.window?.makeKeyAndVisible()
         
         return true
     }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
